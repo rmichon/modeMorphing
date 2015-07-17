bcbc644b011dfadfc1a419228f6fc647f1abf70f
John Granzow <johngranzow@Johns-MacBook-Pro-4.local>
2015-07-16 20:24:01 -0700

    fixed semMid

--- semMidCenter.m
+++ semMidCenter.m
@@ -52,10 +52,10 @@ fm = (im-1)/nbins*fs/2;
 
 nmode = length(fm);
 
-% index = [1 2 3 5 6 7 8 10 11 12 14 15 16 17 18 19 20 21 22 23 24 25 26 27 30 31 32 33 35 36 39 40 41 43 45 46 47 48 49 50 51 52];
-% nmode = length(index);
-% fm = fm(index);
-% gammam = gammam(index);
+index = [1 2 4 5 6 7 8 9 10 11 13 14 15 16 17 18 19 20 21 22 23 24 25 28 30 31 32 33 34 36 37 39 40 41 42 ];
+nmode = length(index);
+fm = fm(index);
+gammam = gammam(index);
 
 figure(3);
 plot(f, 20*log10(abs(irSpectrum)), '-', fm, gammam, 'o'); grid;
